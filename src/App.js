df09105391a6d118f77ebc2e290eaf010604a67c
Sanmandresy <hei.sanda.3@gmail.com>
2023-01-25 11:11:26 +0300

    chore: re-initialize project

--- src/App.js
+++ src/App.js
@@ -1,53 +1,10 @@
-import React,{useState} from "react";
+import React from "react";
 import './App.css';
 
 function App() {
     return(<>
-        <FunctionalComponent/>
-        <ClassComponent/>
-    
+        
     </>);
-}
-
-
-const FunctionalComponent = () => {
-    const [initial,setInitial] = useState(0);
-    return(<>
-        <span className="box">
-            <h1>FunctionalComponent</h1>
-            <p className="box__text">{initial}</p>
-            <button
-                className="incremental__button"
-                onClick={() => setInitial(initial + 1) }>
-                    Increment
-            </button>
-        </span>
-    </>);
-}
-
-class ClassComponent extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            value : 0
-        }
-    }
-
-    render() {
-        return(
-            <span className="box">
-                <h1>ClassComponent</h1>
-                <p className="box__text">{this.state.value}</p>
-                <button
-                    className="incremental__button" 
-                    onClick={() => this.setState((prev) => {
-                     return {value: prev.value + 1}
-                    })}>
-                    Increment
-                </button>
-            </span>
-        );
-    }
 }
 
 export default App;
